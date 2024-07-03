2a99dd7c6fd654dbc70fd5ea23cbb0a3e466e03c
Yiyangse <azziyang@naver.com>
2024-07-03 12:39:04 +0900

    Class Component

---
+++ day5/class-components/src/Greeting.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+interface GreetingProps {
+  name?: string; //?의 의미 nullable 허용한다
+}
+
+interface GreetingState {
+  message: string;
+}
+
+export default class Greeting extends React.Component<
+  GreetingProps,
+  GreetingState
+> {
+  constructor(props: GreetingProps) {
+    super(props);
+    this.state = {
+      message: `Hello from, ${props.name}`,
+    };
+  }
+
+  static getDerivedStateFromProps(props: GreetingProps, state: GreetingState) {
+    console.log(props, state);
+    if (props.name && props.name !== state.message) {
+      const newState = { ...state };
+      newState.message = Greeting.getNewMessage(props.name);
+      return newState;
+    }
+    return state;
+  }
+
+  static getNewMessage(name: string = '') {
+    return `Hello from, ${name}`;
+  }
+
+  render() {
+    console.log('rendering Greeting');
+    if (!this.props.name) {
+      return <div>no name given</div>;
+    }
+
+    return <div>{this.state.message}</div>;
+  }
+}
